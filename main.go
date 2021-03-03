c662d543630d2694a3d0ec2a731d218dc2c22505
Yariv Levy <yalevy@paypal.com>
2021-03-03 22:31:50 +0200

    Initial interface

--- main.go
+++ main.go
@@ -1,6 +1,28 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// ErrIO represents I/O errors: cli, files
+const ErrIO int = 1
 
 func main() {
-    fmt.Println("hello world")
+	
+	if (len(os.Args) != 3) {
+		fmt.Println("USAGE: main $num_processors $filename")
+		os.Exit(ErrIO)
+	}
+
+	filename := os.Args[2]
+	numProcessors, err := strconv.Atoi(os.Args[1]) 
+	if err != nil {
+        fmt.Println(err)
+        os.Exit(ErrIO)
+    }	
+
+	fmt.Printf("Processing %s with %d processors", filename, numProcessors) 
+
 }
