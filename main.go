4ff774016c57e24af549b8b46ca8dab61be7f466
Yariv Levy <yalevy@paypal.com>
2021-03-04 15:11:16 +0200

    Project organization

--- main.go
+++ main.go
@@ -4,10 +4,35 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"bufio"
 )
 
 // ErrIO represents I/O errors: cli, files
 const ErrIO int = 1
+
+func readFile(filename string, numbers *[]int){
+	f, err := os.Open(filename)
+	if err != nil {
+        fmt.Printf("File %s doesn't exist", filename)
+		os.Exit(ErrIO)
+    }
+
+	println(f)
+
+	scanner := bufio.NewScanner(f)
+	i := 0
+	for scanner.Scan() {
+		lineStr := scanner.Text()
+		num, _ := strconv.Atoi(lineStr)
+		if i <= 50 {
+			fmt.Println(lineStr, num)
+			i++
+		}
+		*numbers = append(*numbers, num)
+	}
+
+	fmt.Println("Done reading file")
+}
 
 func main() {
 	
@@ -23,6 +48,13 @@ func main() {
         os.Exit(ErrIO)
     }	
 
-	fmt.Printf("Processing %s with %d processors", filename, numProcessors) 
+	fmt.Printf("Processing %s with %d processors\n", filename, numProcessors) 
 
+	var numbers []int = make([]int, 0, 1000)
+	readFile(filename, &numbers)
+
+	fmt.Println(numbers[0])
+	fmt.Println(numbers[len(numbers)-1])
+	
+	Sort(&numbers)
 }
---
+++ msort.go
@@ -0,0 +1,9 @@
+package main 
+
+import (
+	"fmt"
+)
+
+func Sort(numbers *[]int){
+	fmt.Println("Starting sort")
+}
